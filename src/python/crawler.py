604548058e5ca48c955341ae3c7df2b32c2da64e
gtkacz <gmtkacz@proton.me>
2025-11-12 01:00:54 -0300

    feat: initial NBA data parsing implementation

---
+++ src/python/crawler.py
@@ -0,0 +1,28 @@
+import pathlib  # noqa: CPY001, D100
+
+import pandas as pd
+
+
+def load_team_data(team_name: str, *, data_path: str = "data/html") -> pd.DataFrame:
+    """
+    Load the HTML data for a given team and return it as a DataFrame.
+
+    Args:
+        team_name (str): Name of the team.
+        data_path (str, optional): Path to the directory containing HTML files. Defaults to "data/html".
+
+    Raises:
+        FileNotFoundError: If the specified HTML file does not exist.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the concatenated data from the HTML file.
+    """
+    path = pathlib.Path(f"{data_path}/{team_name}.html")
+
+    if not path.exists():
+        raise FileNotFoundError(f"The file {path} does not exist.")
+
+    with path.open("data/html/Timberwolves.html") as f:
+        data = pd.read_html(f.read())  # pyright: ignore[reportUnknownMemberType]
+
+    return pd.concat((data[0], data[1]))
